59f43463863f6bec5b1e1736ee62080cbfcf8c18
user <user@users-MacBook-Pro.local>
2026-08-28 01:54:36 +0200

    Serve project media in production

--- PrengaConstruction/urls.py
+++ PrengaConstruction/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
